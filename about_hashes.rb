f6f133d4f5d41eb73e47a61dedb80cc85ca61822
Vanessa Anderson <Vanessa.Anderson@teachforamerica.org>
2015-08-07 16:57:12 -0400

    #35 Hashes with Fetch

--- about_hashes.rb
+++ about_hashes.rb
@@ -22,14 +22,16 @@ class AboutHashes < Neo::Koan
 
   def test_accessing_hashes_with_fetch
     hash = { :one => "uno" }
-    assert_equal __, hash.fetch(:one)
-    assert_raise(___) do
+    assert_equal "uno", hash.fetch(:one)
+    assert_raise(KeyError) do
       hash.fetch(:doesnt_exist)
     end
 
     # THINK ABOUT IT:
     #
     # Why might you want to use #fetch instead of #[] when accessing hash keys?
+    # fetch can raise error or return default value, OR execute default code block
+    # [] will return nil if not found. 
   end
 
   def test_changing_hashes
