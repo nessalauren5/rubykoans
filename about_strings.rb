e19be91f1740dfb8723cb7a3ac48ed729c282769
Vanessa Anderson <Vanessa.Anderson@teachforamerica.org>
2015-08-07 20:16:28 -0400

    #67/68 Strings can be split into arrays

--- about_strings.rb
+++ about_strings.rb
@@ -170,13 +170,13 @@ EOS
   def test_strings_can_be_split
     string = "Sausage Egg Cheese"
     words = string.split
-    assert_equal [__, __, __], words
+    assert_equal ["Sausage", "Egg", "Cheese"], words
   end
 
   def test_strings_can_be_split_with_different_patterns
     string = "the:rain:in:spain"
     words = string.split(/:/)
-    assert_equal [__, __, __, __], words
+    assert_equal ["the", "rain", "in", "spain"], words
 
     # NOTE: Patterns are formed from Regular Expressions.  Ruby has a
     # very powerful Regular Expression library.  We will become
