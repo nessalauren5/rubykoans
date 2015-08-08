9b166a6e49514bbddd45418939aabe9e82347dbe
Vanessa Anderson <Vanessa.Anderson@teachforamerica.org>
2015-08-08 14:12:32 -0400

    #121/122 Private methods

--- about_methods.rb
+++ about_methods.rb
@@ -113,14 +113,14 @@ class AboutMethods < Neo::Koan
   private :my_private_method
 
   def test_calling_private_methods_without_receiver
-    assert_equal __, my_private_method
+    assert_equal "a secret", my_private_method
   end
 
   def test_calling_private_methods_with_an_explicit_receiver
-    exception = assert_raise(___) do
+    exception = assert_raise(NoMethodError) do
       self.my_private_method
     end
-    assert_match /__/, exception.message
+    assert_match /private method/, exception.message
   end
 
   # ------------------------------------------------------------------
