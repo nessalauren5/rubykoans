2a2e3d6573f06e353c93d82ce7f9ef0cae74718f
Vanessa Anderson <Vanessa.Anderson@teachforamerica.org>
2015-08-09 10:10:04 -0400

    #211-216 Dice Project: rand num gen

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,9 +2,13 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  #   code ...
+    attr_reader :values
+  def roll(times)
+    @values = Array.new(times){|x| x = 1 + rand(6)}
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
@@ -41,13 +45,14 @@ class AboutDiceProject < Neo::Koan
     second_time = dice.values
 
     assert_not_equal first_time, second_time,
-      "Two rolls should not be equal"
+    "Two rolls should not be equal"
 
     # THINK ABOUT IT:
     #
     # If the rolls are random, then it is possible (although not
     # likely) that two consecutive rolls are equal.  What would be a
     # better way to test this?
+    #statistically**
   end
 
   def test_you_can_roll_different_numbers_of_dice
